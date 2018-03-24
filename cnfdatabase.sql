6da15aeccd02d6a57bfe1275b7756c7fbb924762
Shuaib Kunhabdulla <shuaibabdulla40@gmail.com>
2018-03-25 00:12:09 +0530

    Add files via upload

---
+++ cnfdatabase.sql
@@ -0,0 +1,19 @@
+/*
+SQLyog Enterprise Trial - MySQL GUI v7.11 
+MySQL - 5.5.25a : Database - cnf
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`cnf` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `cnf`;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
